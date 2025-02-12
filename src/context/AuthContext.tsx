ea111d2adf75d97664e43711fb5bf5e1cc29043c
Allan Cabusora <ascex@GLT-MB-1026.local>
2025-02-12 09:26:00 +0800

    v0.0.91

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,44 @@
+import {createContext, useState} from "react";
+import { AuthContextType, AuthProviderProps } from "auth";
+import Cookies from "js-cookie";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+
+const AuthProvider = ({children}: AuthProviderProps) => {
+
+    const [user, setUser] = useState<string | null | undefined>(undefined);
+    const [token] = useState<string | null | undefined>( Cookies.get('laravel_session') ?? undefined);
+    const [isLoggedIn, setIsLoggedIn] = useState(Cookies.get("isLoggedIn") === "true");
+    const [isLoading, setIsLoading] = useState(true);
+
+    const logout = () => {
+        Cookies.remove("isLoggedIn");
+        setUser(undefined);
+        setIsLoggedIn(false);
+    }
+
+    const login = () => {
+        setIsLoggedIn(true);
+    }
+
+    const authValue = {
+        isLoggedIn,
+        isLoading,
+        setIsLoading,
+        setIsLoggedIn,
+        login,
+        logout,
+        token,
+        user
+    }
+
+    return (
+        <AuthContext.Provider value={authValue}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
---
+++ src/types/auth.d.ts
@@ -0,0 +1,22 @@
+import React, {ReactNode} from "react";
+
+declare module 'auth' {
+    export interface AuthProviderProps {
+        children: ReactNode
+    }
+    interface User {
+        id: string;
+        name: string;
+        email: string;
+    }
+
+    export type AuthContextType = {
+        user?: string | null,
+        token?: string | null,
+        isLoggedIn: boolean,
+        isLoading: boolean,
+        setIsLoggedIn:  React.Dispatch<React.SetStateAction<boolean>>,
+        logout: () => void,
+        login: () => void
+    }
+}
